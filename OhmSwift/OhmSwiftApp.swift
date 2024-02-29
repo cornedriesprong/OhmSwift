a17b8c0fe1e96e513b2474ccd9d4a9f07e5c45d4
Corné <corne@cp3.io>
2024-02-29 07:21:39 +0100

    Initial Commit

---
+++ OhmSwift/OhmSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhmSwiftApp.swift
+//  OhmSwift
+//
+//  Created by Corné on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct OhmSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
